d3fc6e0eca3f21d1cd491588e40e4560ec85502f
Filip <filip.horvat997@gmail.com>
2022-03-17 00:33:22 +0100

    test.py: -tkinter button for open file dialog -OpenFile(): plots the signal of .wav audio file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FERI-m12-ROSIS-vaja01
+
 Hitro prototipiranje rešitev; https://vaje.uni-mb.si/vaje/student/vaja.jsp?id=6750
+
+ffmpeg -i "a.wav" -f wav -acodec pcm_s16le -ar 22050 -ac 1 "a.wav"
--- test.py
+++ test.py
@@ -1,4 +1,53 @@
+from tkinter import * 
+from tkinter import filedialog
+import numpy as np
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
+from tkinter.messagebox import showinfo
+
+def OpenFile():
+    filetypes = (
+        ('audio files', '*.wav'),
+        ('All files', '*.*')
+    )
+    filename = filedialog.askopenfilenames(
+        title='Open wav file',
+        initialdir='./',
+        filetypes=filetypes
+    )
+    fs, data = wavfile.read(filename[0])
+    duration = len(data)/fs
+    t = np.arange(0, duration, 1/fs)
+    
+    fig.add_subplot(111).plot(t,data)
+    
+    canvas.draw()
+  
+    canvas.get_tk_widget().pack()
+  
+    toolbar.update()
+
+    canvas.get_tk_widget().pack()
+
+main_window = Tk()  
+main_window.title('Plotting in Tkinter')
+main_window.geometry("500x500")
 
 
-if __name__ == '__main__':
-    print("HEllo worlD")
+fig = Figure(figsize=(5, 4), dpi=100)
+canvas = FigureCanvasTkAgg(fig,master=main_window)  
+
+toolbar = NavigationToolbar2Tk(canvas,main_window)
+
+open_button = Button(master = main_window, 
+                     command = OpenFile,
+                     height = 2, 
+                     width = 10,
+                     text = "Open")
+
+open_button.pack()
+  
+main_window.mainloop()
