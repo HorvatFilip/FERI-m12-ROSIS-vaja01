b5eb22f7aaa38bc23006ce9d3fbcc84aa926da35
Filip <filip.horvat997@gmail.com>
2022-03-18 23:06:45 +0100

    test_class.py: -fixed label signal plot

--- test_class.py
+++ test_class.py
@@ -17,15 +17,13 @@ class Window():
         # Creating the tkinter Window
         self.main_window = Tk()  
         self.main_window.title('Plotting in Tkinter')
-        self.main_window.geometry("500x500")
+        self.main_window.geometry("700x600")
 
-        # Button for closing
-        
         fig, self.fig_axis = plt.subplots(figsize = (5,4), dpi=100)    
+
+        
         self.canvas = FigureCanvasTkAgg(fig,master=self.main_window)  
         self.toolbar = NavigationToolbar2Tk(self.canvas,self.main_window)
-
-
 
         self.open_button = Button(master = self.main_window, 
                              command = self.OpenFile,
@@ -34,7 +32,6 @@ class Window():
                              text = "Open")
 
         self.open_button.pack()
-
   
         self.main_window.mainloop()
   
@@ -48,22 +45,30 @@ class Window():
                 initialdir='./',
                 filetypes=filetypes
             )
+            if not filename:
+                return
             fs, data = wavfile.read(filename[0])
             duration = len(data)/fs
             t = np.arange(0, duration, 1/fs)
 
-            self.fig_axis.set_title(filename[0])
-            self.fig_axis.set_ylabel('Amplitude')
-            self.fig_axis.set_xlabel('Time [s]')
+           
             plt.clf()
+            title = filename[0].split('/')
+            plt.title(title[-1])
+            plt.xlabel("Cas [s]")
+            plt.ylabel("Amplituda")
             plt.plot(t,data)
 
+           
             self.canvas.draw()
             self.canvas.get_tk_widget().pack()
         
             self.toolbar.update()
 
             self.canvas.get_tk_widget().pack()
+            
+            plt.tight_layout()
+            
             
 
     def Close(self):
