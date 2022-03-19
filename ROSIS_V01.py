b5b795ebbb5ff258fc33527935981ab3639ba875
Filip <filip.horvat997@gmail.com>
2022-03-19 21:53:28 +0100

    test_class.py: -renamed to ROSIS_V01.py -changed initial openfiledialog dir

--- ROSIS_V01.py
+++ ROSIS_V01.py
@@ -1,21 +1,28 @@
-from tkinter import Tk, Button, Label, filedialog
-import numpy as np
+from tkinter import Tk, Button, filedialog
 from scipy.io import wavfile
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
-NavigationToolbar2Tk)
-from tkinter.messagebox import showinfo, askokcancel
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, 
+    NavigationToolbar2Tk
+)
+# Class for tkinter window
+  
   
 class Window():
     def __init__(self):
+  
+        # Creating the tkinter Window
         self.main_window = Tk()  
         self.main_window.title('Plotting in Tkinter')
-        self.main_window.geometry("500x500")
+        self.main_window.geometry("700x600")
 
-        self.fig, self.fig_axis = plt.subplots(figsize = (5,4), dpi=100)
-        self.canvas = FigureCanvasTkAgg(self.fig,master=self.main_window)  
+        fig, self.fig_axis = plt.subplots(figsize = (5,4), dpi=100)    
 
+        
+        self.canvas = FigureCanvasTkAgg(fig,master=self.main_window)  
         self.toolbar = NavigationToolbar2Tk(self.canvas,self.main_window)
 
         self.open_button = Button(master = self.main_window, 
@@ -25,42 +32,49 @@ class Window():
                              text = "Open")
 
         self.open_button.pack()
-
-
-        self.main_window.protocol("WM_DELETE_WINDOW", self.CloseApp)
+  
         self.main_window.mainloop()
-
-        def OpenFile(self):
+  
+    def OpenFile(self):
             filetypes = (
                 ('audio files', '*.wav'),
                 ('All files', '*.*')
             )
             filename = filedialog.askopenfilenames(
                 title='Open wav file',
-                initialdir='./',
+                initialdir='./audiofiles/',
                 filetypes=filetypes
             )
+            if not filename:
+                return
             fs, data = wavfile.read(filename[0])
             duration = len(data)/fs
             t = np.arange(0, duration, 1/fs)
 
-            self.fig_axis.set_title(filename[0])
-            self.fig_axis.set_ylabel('Amplitude')
-            self.fig_axis.set_xlabel('Time [s]')
+           
             plt.clf()
+            title = filename[0].split('/')
+            plt.title(title[-1])
+            plt.xlabel("Cas [s]")
+            plt.ylabel("Amplituda")
             plt.plot(t,data)
 
-            
+           
             self.canvas.draw()
-        
             self.canvas.get_tk_widget().pack()
         
             self.toolbar.update()
 
             self.canvas.get_tk_widget().pack()
+            
+            plt.tight_layout()
+            
+            
 
-        def CloseApp(self):
-            if askokcancel("Quit", "Do you want to quit?"):
-                self.main_window.destroy()
-
-win = Window()
+    def Close(self):
+        #plt.close(self.fig)
+        self.main_window.destroy()
+  
+  
+# Running test window
+test = Window()
