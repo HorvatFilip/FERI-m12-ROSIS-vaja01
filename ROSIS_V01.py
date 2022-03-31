d7667d0e7e17872e85a5fd2ca267949d0d118294
HorvatFilip <filip.horvat997@gmail.com>
2022-04-01 01:33:50 +0200

    added better screenshots for new report

--- ROSIS_V01.py
+++ ROSIS_V01.py
@@ -12,6 +12,7 @@ from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, 
     NavigationToolbar2Tk
 )
+import sounddevice as sd
 
 
 class Window():
@@ -44,90 +45,107 @@ class Window():
                              text = "Open")
 
         self.open_button.grid(column=0, row=0)
+
+        self.record_button = Button(master = self.main_window, 
+                             command = self.RecordAudio,
+                             height = 2, 
+                             width = 10,
+                             text = "Rec")
+        self.record_button.grid(column=1, row=0)
   
         self.main_window.mainloop()
   
+    def RecordAudio(self):
+        sample_freq = 44400
+        duration = 3
+        recording = sd.rec(int(duration * sample_freq),samplerate = sample_freq, channels = 2)
+        sd.wait()
+        wavfile.write("custom_recording.wav", sample_freq, recording)
+
+        fs, samples = wavfile.read("custom_recording.wav")
+        self.UpdatePlots("custom_recording.wav", fs, samples)
+
     def OpenFile(self):
-            filetypes = (
-                ('audio files', '*.wav'),
-                ('All files', '*.*')
-            )
-            filename = filedialog.askopenfilenames(
-                title='Open wav file',
-                initialdir='./audiofiles/',
-                filetypes=filetypes
-            )
-            if not filename:
-                return
-            title = filename[0].split('/')
-            
-            #
-            # SIGNAL
-            #
-            fs, samples = wavfile.read(filename[0])
-            duration = len(samples)//fs
-            t = np.arange(0, duration, 1/fs)
-           
-            self.fig1.clf()
-            self.fig_axis1 = self.fig1.add_subplot(111)
-            self.fig_axis1.set_title(title[-1])
-            self.fig_axis1.set_xlabel("Cas [s]")
-            self.fig_axis1.set_ylabel("Amplituda")
-            self.fig_axis1.plot(t,samples)
-            self.fig1.tight_layout()
-           
-            self.canvas1.draw()
-            self.canvas1.get_tk_widget().grid(row=1, column=0)
-            self.toolbar_frame1.grid(row=2, column=0)
-            if not self.toolbar1:
-                self.toolbar1 = NavigationToolbar2Tk(self.canvas1,self.toolbar_frame1)
-            self.toolbar1.update()
-            self.canvas1.get_tk_widget().grid(row=1, column=0)
+        filetypes = (
+            ('audio files', '*.wav'),
+            ('All files', '*.*')
+        )
+        filename = filedialog.askopenfilenames(
+            title='Open wav file',
+            initialdir='./audiofiles/',
+            filetypes=filetypes
+        )
+        if not filename:
+            return
+
+        fs, samples = wavfile.read(filename[0])
+        self.UpdatePlots(filename, fs, samples)
+        
+    def UpdatePlots(self, filename, fs, samples):
+        title = filename[0].split('/')
+        #
+        # SIGNAL
+        #
+
+        duration = len(samples)//fs
+        t = np.arange(0, duration, 1/fs)
+       
+        self.fig1.clf()
+        self.fig_axis1 = self.fig1.add_subplot(111)
+        self.fig_axis1.set_title(title[-1])
+        self.fig_axis1.set_xlabel("Cas [s]")
+        self.fig_axis1.set_ylabel("Amplituda")
+        self.fig_axis1.plot(t,samples)
+        self.fig1.tight_layout()
+       
+        self.canvas1.draw()
+        self.canvas1.get_tk_widget().grid(row=1, column=0)
+        self.toolbar_frame1.grid(row=2, column=0)
+        if not self.toolbar1:
+            self.toolbar1 = NavigationToolbar2Tk(self.canvas1,self.toolbar_frame1)
+        self.toolbar1.update()
+        self.canvas1.get_tk_widget().grid(row=1, column=0)
+        #
+        # DFT
+        #
+        y_fft = fft(samples)
+        x_fft = fftfreq(fs * duration, 1 / fs)
+        self.fig2.clf()
+        self.fig_axis2 = self.fig2.add_subplot(111)
+        self.fig_axis2.set_title(title[-1])
+        self.fig_axis2.set_xlabel("Frekvenca [Hz]")
+        self.fig_axis2.set_ylabel("Amplituda")
+        self.fig_axis2.plot(x_fft[:len(x_fft)//2], np.abs(y_fft[:len(y_fft)//2]))
+        self.fig2.tight_layout()
+       
+        self.canvas2.draw()
+        self.canvas2.get_tk_widget().grid(row=1, column=1)
+        self.toolbar_frame2.grid(row=2, column=1)
+        if not self.toolbar2:
+            self.toolbar2 = NavigationToolbar2Tk(self.canvas2,self.toolbar_frame2)
+        self.toolbar2.update()
+        self.canvas2.get_tk_widget().grid(row=1, column=1)
 
 
-            #
-            # DFT
-            #
-            y_fft = fft(samples)
-            x_fft = fftfreq(fs * duration, 1 / fs) 
-
-            self.fig2.clf()
-            self.fig_axis2 = self.fig2.add_subplot(111)
-            self.fig_axis2.set_title(title[-1])
-            self.fig_axis2.set_xlabel("Frekvenca [Hz]")
-            self.fig_axis2.set_ylabel("Amplituda")
-            self.fig_axis2.plot(x_fft, np.abs(y_fft[:len(y_fft)//2]))
-            self.fig2.tight_layout()
-           
-            self.canvas2.draw()
-            self.canvas2.get_tk_widget().grid(row=1, column=1)
-            self.toolbar_frame2.grid(row=2, column=1)
-            if not self.toolbar2:
-                self.toolbar2 = NavigationToolbar2Tk(self.canvas2,self.toolbar_frame2)
-            self.toolbar2.update()
-            self.canvas2.get_tk_widget().grid(row=1, column=1)
-
-
-            #
-            # KRATKO ČASOVNA FOURIEROVA TRANS
-            #
-            freq, t, spectogram = signal.spectrogram(samples.T[0], fs)
-
-            self.fig3.clf()
-            self.fig_axis3 = self.fig3.add_subplot(111)
-            self.fig_axis3.set_title(title[-1])
-            self.fig_axis3.set_xlabel("Cas [s]")
-            self.fig_axis3.set_ylabel("Frekvenca [Hz]")
-            self.fig_axis3.specgram(samples.T[0], NFFT=1024, Fs=fs)
-            self.fig3.tight_layout()
-           
-            self.canvas3.draw()
-            self.canvas3.get_tk_widget().grid(row=3, column=0)
-            self.toolbar_frame3.grid(row=4, column=0)
-            if not self.toolbar3:
-                self.toolbar3 = NavigationToolbar2Tk(self.canvas3,self.toolbar_frame3)
-            self.toolbar3.update()
-            self.canvas3.get_tk_widget().grid(row=3, column=0)
+        #
+        # KRATKO ČASOVNA FOURIEROVA TRANS
+        #
+        freq, t, spectogram = signal.spectrogram(samples.T[0], fs)
+        self.fig3.clf()
+        self.fig_axis3 = self.fig3.add_subplot(111)
+        self.fig_axis3.set_title(title[-1])
+        self.fig_axis3.set_xlabel("Cas [s]")
+        self.fig_axis3.set_ylabel("Frekvenca [Hz]")
+        self.fig_axis3.specgram(samples.T[0], NFFT=1024, Fs=fs)
+        self.fig3.tight_layout()
+        
+        self.canvas3.draw()
+        self.canvas3.get_tk_widget().grid(row=3, column=0)
+        self.toolbar_frame3.grid(row=4, column=0)
+        if not self.toolbar3:
+            self.toolbar3 = NavigationToolbar2Tk(self.canvas3,self.toolbar_frame3)
+        self.toolbar3.update()
+        self.canvas3.get_tk_widget().grid(row=3, column=0)
             
 
     def Close(self):
